77ef3e7f0faf560ec179ace46a20542d0a2cf83b
Gulshan Khandale <gulshankhandale61@gmail.com>
2025-04-10 23:31:03 +0530

    delete files

--- codeongo/CarView.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-struct CarView: View {
-    var body: some View {
-        Image("carImage")
-            .resizable()
-            .aspectRatio(contentMode: .fill)
-    }
-}
--- codeongo/NetworkManager.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-class NetworkManager {
-    func fetchData() {
-        print("Fetching data...")
-    }
-}
